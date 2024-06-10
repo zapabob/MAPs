43c191d39ef33e73d11d60d767848f9dfdfac70c
峯岸　亮 <1920071390@campus.ouj.ac.jp>
2024-06-10 11:24:47 +0900

    Add files via upload

--- env.ts
+++ env.ts
@@ -1,5 +1,12 @@
-// server.ts
 import dotenv from 'dotenv';
 dotenv.config();
 
-// アプリケーションのコード
+const OPENAI_API_KEY: string = process.env.OPENAI_API_KEY || '';
+const GOOGLE_API_KEY: string = process.env.GOOGLE_API_KEY || '';
+const ANTHROPIC_API_KEY: string = process.env.ANTHROPIC_API_KEY || '';
+const LINE_API_KEY: string = process.env.LINE_API_KEY || '';
+const YOUTUBE_API_KEY: string = process.env.YOUTUBE_API_KEY || '';
+const X_API_KEY: string = process.env.X_API_KEY || '';
+const MICROSOFT_API_KEY: string = process.env.MICROSOFT_API_KEY || '';
+
+// アプリケーションのコードを実行する際に必要な環境変数
